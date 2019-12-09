75c4c2b058ae1092ad227b52903b1b665557da0d
Prashant <ppresh.ant@gmail.com>
2019-12-08 22:50:14 -0600

    plotting pRV01, pInt8 and controls on same plot - Work in progress - Writing a generaliable input into plot_time_series function

--- general_functions_plate_reading.R
+++ general_functions_plate_reading.R
@@ -146,8 +146,10 @@ mutate_cond <- function(.data, condition, ..., envir = parent.frame())
 # formatting plots ----
 
 # plotting timeseries (mean in points, stdev in errorbars; Coloured by reporter plasmid, facetted by integrase plasmid and shape as inducer)
-plot_time_series <- function(data_table, induction_duration = c(0,6/24), x_breaks = c(0,6,24,48), stroke_width = 1, x_axis_label = 'Time (days)', y_axis_label = 'GFP/OD (a.u.)', plot_title = 'AHL flipping with time' )
+plot_time_series <- function(data_table, induction_duration = c(0,6/24), x_breaks = c(0,6,24,48), stroke_width = 1, x_axis_label = 'Time (days)', y_axis_label = 'GFP/OD (a.u.)', plot_title = 'AHL flipping with time', colour_by_var = Reporter, facet_by_var = Samples )
 {
+  colour_by_var <- enquo(colour_by_var) 
+  facet_by_var <- enquo(facet_by_var)
   plt <- ggplot(data_table, aes(Time, mean, colour = Reporter, shape = Inducer)) + 
     annotate('rect', xmin = induction_duration[1], ymin = 0, xmax = induction_duration[2], ymax = Inf, alpha = .2) +  # grey rectangle for induction duration
     geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd), width = 0.25) + facet_wrap(~ Samples) + geom_line() + geom_point(size = 2, fill = 'white', stroke = stroke_width) + 
