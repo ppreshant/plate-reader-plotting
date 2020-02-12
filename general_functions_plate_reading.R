356e438841270454407389e25245434720bdf1f8
Prashant <ppresh.ant@gmail.com>
2020-02-12 08:52:33 -0600

    Adding dose response plot - Copied from S010 and modified - have to make time discrete and plot as a colour - Have to add hill function fits and add to plot

--- general_functions_plate_reading.R
+++ general_functions_plate_reading.R
@@ -147,6 +147,20 @@ plot_time_series <- function(data_table, induction_duration = c(0,1), x_breaks =
    format_classic(plt) # output a classic formatted plot
 }
 
+# plotting dose response (mean vs Inducer)
+plot_dose_response <- function(sel_tablex, y_axis_label = 'GFP/OD (a.u.)', plot_title = 'Flipping vs inducer dose' )
+{ # Input the filtered summary table and plot the mean vs Inducer points and errorbars. facet by Samples, colour by sample and title
+  plt1 <- ggplot(sel_tablex, aes(Inducer, mean)) + 
+    geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd), width = 0.25) + 
+    geom_point(size = 2, fill = 'white') + 
+    facet_grid(~ Samples, scales = 'free_x', space = 'free_x') +
+    ylab(y_axis_label) + ggtitle(plot_title) +
+    scale_x_log10(  # logscale for y axis with tick marks
+      labels = scales::trans_format("log10", scales::math_format(10^.x) ) )
+  
+  format_classic(plt1) # output a classic formatted plot
+}
+
 # formatting labels in logscale cleanly : a x 10^b
 # use as ggplot(df,aes(x,y)) + geom_point() + scale_y_log10(labels = fancy_scientific)
 fancy_scientific <- function(l) {
