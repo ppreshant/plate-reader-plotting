bba89cfd6b6b32a13784a684cf9915d1ba90c86d
Prashant <ppresh.ant@gmail.com>
2020-03-20 02:01:41 -0500

    fixed inducer duration, facetting by grid

--- general_functions_plate_reading.R
+++ general_functions_plate_reading.R
@@ -166,13 +166,13 @@ baseline_subtraction <- function(data1, baseline_name) # work in progress
 # formatting plots ----
 
 # plotting timeseries (mean in points, stdev in errorbars; Coloured by reporter plasmid, facetted by integrase plasmid and shape as inducer)
-plot_time_series <- function(data_table, induction_duration = c(0,6/24), x_breaks = c(0,6,24,48), stroke_width = 1, x_axis_label = 'Time (days)', y_axis_label = 'GFP/OD (a.u.)', plot_title = 'AHL flipping with time', colour_by_var = Reporter, facet_by_var = Samples )
+plot_time_series <- function(data_table, induction_duration = c(1,2), x_breaks = c(0,6,24,48), stroke_width = 1, x_axis_label = 'Time (days)', y_axis_label = 'GFP/OD (a.u.)', plot_title = 'AHL flipping with time', colour_by_var = Reporter, facet_by_var = Samples )
 {
   colour_by_var <- enquo(colour_by_var) 
   facet_by_var <- enquo(facet_by_var)
   plt <- ggplot(data_table, aes(Time, mean, colour = Reporter, shape = Inducer)) + 
     annotate('rect', xmin = induction_duration[1], ymin = 0, xmax = induction_duration[2], ymax = Inf, alpha = .2) +  # grey rectangle for induction duration
-    geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd), width = 0.25) + facet_wrap(Media ~ Samples) + geom_line() + geom_point(size = 2, fill = 'white', stroke = stroke_width) + 
+    geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd), width = 0.25) + facet_grid(Media ~ Samples) + geom_line() + geom_point(size = 2, fill = 'white', stroke = stroke_width) + 
     scale_shape_manual(values = c(21,19)) +  scale_x_continuous(breaks = x_breaks) + 
     ylab(y_axis_label) + xlab(x_axis_label) + ggtitle(plot_title)
  
