f3dbecf77d61a03bbb1ebcc156029cf7523b0e35
ppreshant <ppresh.ant@gmail.com>
2021-06-01 19:08:04 -0500

    4-plotting_fns : adding plot_time_series from S022 - removed format_classic commands since it is set universally now

--- scripts_general_functions/4-plotting_fns.R
+++ scripts_general_functions/4-plotting_fns.R
@@ -16,5 +16,20 @@ plot_dose_response <- function(sel_tablex, y_axis_label = 'GFP/OD (a.u.)', plot_
     facet_grid(~ category, scales = 'free_x', space = 'free_x') +
     ylab(y_axis_label) + ggtitle(plot_title) 
   
-  format_classic(plt1) %>% format_logscale_x # output a classic formatted plot with logscale x
+  plt1 %>% format_logscale_x # output a classic formatted plot with logscale x
 }
+
+
+# untested; copied from S022
+# plotting timeseries (mean in points, stdev in errorbars; Coloured by reporter plasmid, facetted by integrase plasmid and shape as inducer)
+plot_time_series <- function(data_table, induction_duration = c(0,1), x_breaks = c(0,6,24,48), stroke_width = 1, x_axis_label = 'Time (days)', y_axis_label = 'GFP/OD (a.u.)', plot_title = 'AHL flipping with time' )
+{
+  plt <- ggplot(data_table, aes(Time, mean, colour = Inducer, shape = Reporter)) + 
+    annotate('rect', xmin = induction_duration[1], ymin = 0, xmax = induction_duration[2], ymax = Inf, alpha = .2) +  # grey rectangle for induction duration
+    geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd), width = 0.05) + facet_wrap(~ Samples) + geom_line() + geom_point(size = 2, fill = 'white', stroke = stroke_width) + 
+    scale_x_continuous(breaks = x_breaks) + 
+    ylab(y_axis_label) + xlab(x_axis_label) + ggtitle(plot_title)
+  
+  format_classic(plt) # output a classic formatted plot
+}
+
