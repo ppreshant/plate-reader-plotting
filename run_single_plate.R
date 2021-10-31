ac0edfa5284a6410b91ac3f041089896914909cc
ppreshant <ppresh.ant@gmail.com>
2021-10-31 00:16:51 -0500

    run_single_plate.R : added MFDpir to baseline - renamed processed.data to processed_and_baseline_list - saving processed data as a csv file for easy retrieval, further analysis and easy merging with other data-sets and for publication plots with specific scripts - pointing to example file for easy initiation for new users

--- run_single_plate.R
+++ run_single_plate.R
@@ -3,7 +3,7 @@
 # User inputs ----
 
 # User inputs: 1. Enter name of the excel file, 2. Name of the data sheet(S) 3. number of rows and columns in plate reader data 4. Title for plots #comment (file name starts in the previous directory of this Rproject)
-flnm <- 'S033_ww-isolts_AHL-gfp_23-7-21'
+flnm <- 'example-S032_Mfdpir_pPK66,sPK14,15_mScarlet_leak_check'
 # Note: The script only works for SPARK files where OD, GFP and RFP are read, 
   # if you leave out anything b_gap needs to be changed accordingly
 sheet_name <- 'default'
@@ -14,7 +14,7 @@ n_Rows <- 2;
 n_Cols <- 9;
 partial_plate = T; # enter FALSE (F) here when the plate is 8 x 12 but non rectangular
 
-baseline_sample_to_subtract <- 'MG1655|DH10B|NEB10b|PBS' # Add baseline cell name(s) here
+baseline_sample_to_subtract <- 'MG1655|DH10B|MFDpir|NEB10b|PBS' # Add baseline cell name(s) here
 # Fluorescence from samples matching this name will be subtracted from all values (baseline)
 
 # Prelims ----
@@ -32,9 +32,13 @@ fl <- read_plateReader_file(flpath) # load all non empty sheets of the excel fil
 # 2. Retrieve and merge the grids to associate metadata for respective samples
 # 3. Calculate ratios of fluorescence to OD, calculate mean
 # 4. Create a number for replicates (same name, and metadata)
+# 5. also returns the baseline data - to show in the markdown
 
-processed.data <- read_multiple_grids_in_sheet(sheet_name)
+processed_and_baseline_list <- read_multiple_grids_in_sheet(sheet_name)
 
+# unpack the processed data and baseline
+processed.data <- processed_and_baseline_list[[1]]
+empty_cells_baseline <- processed_and_baseline_list[[2]]
 
 # Processing ----
 # location for optional processing - custom written code
@@ -45,4 +49,12 @@ processed.data <- read_multiple_grids_in_sheet(sheet_name)
 # plotting ----
 
 # calling r markdown file for plotting in a nice format html file
-rmarkdown::render('plate reader plotting.Rmd', output_file = str_c('./html files/', title_name, '.html'))
+rmarkdown::render('plate_reader_plotting_and_html.Rmd', output_file = str_c('./html files/', title_name, '.html'))
+
+
+# Save data ----
+
+# save data in the 'processed' folder
+write.csv(processed.data, 
+          str_c('../plate reader data/processed/', flnm, '-processed.csv', 
+                na = ''))
