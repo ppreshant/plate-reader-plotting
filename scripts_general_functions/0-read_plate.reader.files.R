3bc20acd2f2242bc945171eb3920d288e019eabd
ppreshant <ppresh.ant@gmail.com>
2021-10-31 00:14:40 -0500

    0-read_plate.reader.files : Generalizing when there is a partial grid (Infinite M1000) accounting for borders of the excel file

--- scripts_general_functions/0-read_plate.reader.files.R
+++ scripts_general_functions/0-read_plate.reader.files.R
@@ -17,7 +17,8 @@ read_plateReader_file <- function(flnm)
 
 map_in_sheet <- function(data_list, match.key, 
                          match.col.number, neighbour.col.number = match.col.number + 1)
-{ # finds the occurrence of "match.key" in the match.col.number (generally 1st column) of data list and gives the row of occurrence along with the index
+{ # finds the occurrence of "match.key" in the match.col.number (generally 1st column) of data list and ... 
+  # ... gives the row of occurrence along with the index
   
   data_list %<>% mutate(index = 1:n())  # add a column for row index
   
@@ -38,22 +39,33 @@ find_plate_read_grid <- function(.df, start_row_index, start_col_index = 1)
 { 
   # add an error check if first cell is not <>
   
+  # Check if the excel file holds a full 9 x 13 grid (8 x 12 + row and col labels) at this location
+  # relevant for Inifinite M1000 grids that are smaller than 8 x 12
+  grid_end_indices <- pmin( c(start_row_index + 8, start_col_index + 12), # store the minimum of a ..
+                           dim(.df))   # .. full grid or the excel dimensions
   
-  # 9 columns x 13 rows including labels = theoretical maximum 96 well plate 
-  full_grid <- .df[start_row_index + 0:8, start_col_index + 0:12]
+  # find indices relative to grid : so 8 x 12 grid will become 10 x 14 (including the headers and 1 border row/col)
+  relative_grid_border_indices <- grid_end_indices - c(start_row_index, start_col_index) + c(2,2)
+  
+  
+  # Read 9 columns x 13 rows including labels = theoretical maximum 96 well plate 
+  full_grid <- .df[start_row_index : grid_end_indices[1],  # extract a full grid or smaller until end of sheet
+                   start_col_index : grid_end_indices[2] ]
+  
   
   # find the empty cells around the edges of the grid using the headers for rows and cols
   col_edge <- full_grid[1,] %>% # select first row (<>, 1, 2, 3..)
     {which(is.na(.))} %>%  # find the empty cells (NA) in the grid header
-    min(10) # find the minimum NA occurrence or the cell beyond the full grid
+    min(., relative_grid_border_indices[2]) - 2 # find the minimum NA occurrence or the cell beyond the full grid
   
   row_edge <- full_grid[,1] %>% # select first col (<>, A, B, C..)
     {which(is.na(.))} %>%  # find the empty cells (NA) in the row headers
-    min(14) # find the minimum NA occurrence or the cell beyond the full grid
+    min(., relative_grid_border_indices[1]) - 2 # find the minimum NA occurrence or the cell beyond the full grid
+   # should the 1 and 2 be interchanged? 
   
   # read the actual grid (< = full grid)
   if(col_edge == 10 & row_edge == 14) {data_grid <- full_grid # if it's a full grid
-  }  else data_grid <- .df[start_row_index + 0:(row_edge-2), start_col_index + 0:(col_edge-2)]
+  }  else data_grid <- .df[start_row_index + 0:(row_edge), start_col_index + 0:(col_edge)]
   
 }
 
