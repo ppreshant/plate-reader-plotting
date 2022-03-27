6119204c23b21d0d0623d2c53aa86fe998e1e21a
ppreshant <ppresh.ant@gmail.com>
2022-03-26 22:20:27 -0500

    3-mathematical.functions : linear regression fit coeficients, copied from COVID-qPCR/g.5

--- scripts_general_functions/3-mathematical.functions.R
+++ scripts_general_functions/3-mathematical.functions.R
@@ -50,3 +50,26 @@ hill_fit.SS <- function(.data)
   #              data = .data)
   
 }
+
+
+# linear regression equation ----
+
+# Extract linear regression equation and parameters from the fit by lm
+# Source: https://stackoverflow.com/a/7549819/9049673
+# copied from COVID-qPCR repo/g.5
+
+lm_eqn <- function(m, trig = 0){
+  # use trig = 'coeff' to output data frame of the fitting coefficients 
+  # trig = 0 for output of equation (for pasting on the plot)
+  
+  # format the equation "y = mx + c, R^2 = Rsquare value" as text to put on plot
+  eq <- substitute(italic(y) == b %.% italic(x)+ a*","~~italic(r)^2~":"~r2, 
+                   list(a = format(unname(coef(m)[1]), digits = 4), 
+                        b = format(unname(coef(m)[2]), digits = 3), 
+                        r2 = format(summary(m)$r.squared, digits = 3)))
+  
+  # output a table with slope, y_intercept and r_square rounded off
+  if(trig == 'coeff') tibble(slope = round(coef(m)[2], 2), y_intercept = round(coef(m)[1], 2), r_square = round(summary(m)$r.squared, 3))
+  
+  else as.character(as.expression(eq)); 
+}
