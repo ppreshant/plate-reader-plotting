9c4573d63482b0026ed8ca278b86bdba549f7e61
ppreshant <ppresh.ant@gmail.com>
2023-02-02 19:38:57 -0600

    Generalized plot_static_fluorescence to work with fl/OD kind of channels, other datasets and optional interactive plots

--- scripts_general_functions/9-plot_static_fluorescence.R
+++ scripts_general_functions/9-plot_static_fluorescence.R
@@ -2,26 +2,26 @@
 
 ## WORK IN PROGRESS
 
-plot_raw_static_fluorescence <- function(.fluor_variable)
+plot_static_fluorescence <- function(.fluor_variable, .data = processed.data, interactive_plot = TRUE)
 {
   # Parse exprs ----
   
   # Make an expression for fluor variable with "_mean"
-  fluor_w_mean_expr <- rlang::parse_expr(str_c(rlang::expr_text(enexpr(.fluor_variable)), '_mean'))
+  fluor_w_mean_expr <- rlang::sym(str_c(deparse(enexpr(.fluor_variable)), '_mean'))
   fluor_str <- rlang::expr_text(enexpr(.fluor_variable))
   
   
   # process data ---- 
   
   # process data into order (plots in descending order)
-  fluor_order_processed.data <- processed.data %>% 
+  fluor_order_data <- .data %>% 
     arrange({{.fluor_variable}}) %>% 
     
     ungroup() %>% 
     mutate(across(Samples, fct_inorder))
   
   # Select dataset of only mean samples
-  fluor_unique_mean <- fluor_order_processed.data %>% 
+  fluor_unique_mean <- fluor_order_data %>% 
     select(Samples, contains('mean')) %>% 
     unique()
   
@@ -29,7 +29,7 @@ plot_raw_static_fluorescence <- function(.fluor_variable)
   # Plot ----
   
   # plot a horizontal bar graph, along with individual replicates and mean annotated
-  plt_fluor <- ggplot(data = fluor_order_processed.data,
+  plt_fluor <- ggplot(data = fluor_order_data,
                     mapping = aes(y = Samples, x = {{.fluor_variable}})) + 
     geom_jitter(width = 0, height = .3) + # plot individual replicates
     
@@ -47,28 +47,28 @@ plot_raw_static_fluorescence <- function(.fluor_variable)
     ggtitle('Fluorescence measurement',
             subtitle = 'Plasmids')
   
-  
   # Add mean and text labels ----
   
-  {plt_fluor + 
-      
-      # plot mean as a vertical dash (for small point, use size = 0.5)
-      geom_point(data = fluor_unique_mean, 
-                 aes(x = {{fluor_w_mean_expr}}), size = 5, shape = '|') +
-      
-      # add a text label for quick reference of the mean
-      geom_text(data = fluor_unique_mean,  
-                mapping = aes(x = {{fluor_w_mean_expr}}, label = {{fluor_w_mean_expr}} %>% round, 
-                              hjust = if_else({{fluor_w_mean_expr}} > max({{fluor_w_mean_expr}})/2, 1.3, -0.3)))
+  labelled_plot <- 
+    
+    {plt_fluor + 
+        
+        # plot mean as a vertical dash (for small point, use size = 0.5)
+        geom_point(data = fluor_unique_mean, 
+                   aes(x = {{fluor_w_mean_expr}}), size = 5, shape = '|') +
+        
+        # add a text label for quick reference of the mean
+        geom_text(data = fluor_unique_mean,  
+                  mapping = aes(x = {{fluor_w_mean_expr}}, label = {{fluor_w_mean_expr}} %>% round, 
+                                hjust = if_else({{fluor_w_mean_expr}} > max({{fluor_w_mean_expr}})/2, 1.3, -0.3)))
     } %>% 
     print()
   
   # ggsave(str_c('./plots and data/', title_name, '.png'), width = 5, height = 4) # save plot if necessary
-  
-  
-  # interactive plot ---- 
-  
-  plotly::ggplotly(plt_fluor) %>% plotly::style(textposition = 'top right')
+
+  # interactive plot (optional) ---- 
+  if(interactive_plot) {plotly::ggplotly(plt_fluor) %>% plotly::style(textposition = 'top right')
+    } else labelled_plot # else returns the labelled plot
   
   
   
