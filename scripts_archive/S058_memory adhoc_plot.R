3d342dc52af1d082ade19bbc014883476bc7dbf2
ppreshant <ppresh.ant@gmail.com>
2023-02-27 13:01:51 -0600

    Changing scales to 'free_y' and adding annotation rectangle for induction

--- scripts_archive/S058_memory adhoc_plot.R
+++ scripts_archive/S058_memory adhoc_plot.R
@@ -1,5 +1,9 @@
 # S058 plotting
 
+# Run `run_single_plate.R till processed.data or load from the processed.data csv file`
+
+
+
 # data polishing ----
 
 # extrapolate inducer for d-1 to connect the point to initial day --- or would this be misleading?
@@ -8,6 +12,7 @@
 
 processed.data %<>% 
   separate(Samples, into = c('plasmid', 'organism'), remove = F) # split Samples a/b..
+  # mutate(across(day, as.numeric)) # convert day into numeric
 
   # filter(day == -1) %>% view
 # work in progress
@@ -19,13 +24,14 @@ processed.data %<>%
 plt_panels <- ggplot(processed.data,
        aes(x = day, y = `RFP/OD`, colour = AHL_uM)) + 
   geom_point() + geom_line(aes(group = AHL_uM)) + 
+  annotate(geom = 'rect', xmin = 1, xmax = 2, ymin = -Inf, ymax = Inf, alpha = 0.2) + # show induction, day is factor
   
   ggtitle(title_name) + 
   theme(legend.position = 'top') + 
-  facet_wrap(facets = vars(Samples), ncol = 4)
+  facet_wrap(facets = vars(Samples), ncol = 4, scales = 'free_y') # zoom within every plot 'free_y' scale
 
 # save plot 
-ggsave(plot_as(title_name), plt_panels, width = 4, height = 6)
+ggsave(plot_as(title_name, '-zoomed'), plt_panels, width = 6, height = 6) # increase widthe 4 -> 6 for 'free_y'
 
 
 # combine plot
