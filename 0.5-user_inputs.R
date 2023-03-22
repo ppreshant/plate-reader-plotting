5f43ba4ae5a8ef5e1a99893e3c6c2d832a575492
ppreshant <ppresh.ant@gmail.com>
2023-03-21 19:15:51 -0500

    Autodetect and remove date from flnm to make title_name

--- 0.5-user_inputs.R
+++ 0.5-user_inputs.R
@@ -9,17 +9,22 @@
 # Note:  (file name starts in the previous directory of this Rproject)
 # Note: The script only works for SPARK files where OD, metadata next to it, and optionally any fluorescence below it are read
 
-flnm <- 'S038_47+67_23-2-22'
+flnm <- 'S066-67_Ara-d0_memory WW-d0_18-3-23'
 
 sheet_name <- 'default' # 'default' reads the first sheet (for single plate runs only)
 
-title_name <- flnm # appears on the html file name and header of selected plots, change as required
-
 
 # for single plate runs only (I think, need to check)
-
 baseline_sample_to_subtract <- 'MG1655|DH10B|MFDpir|NEB10b|PBS' # Add baseline cell name(s) here
 # Fluorescence from samples matching this name will be subtracted from all values (baseline)
 
 # should I divide the signal by molecular equivalent fluorophores? 
 do_MEFL_normalization <- F # TRUE by default, unless you want to turn off
+
+
+# Calculation ----
+
+date_regex <- '[:digit:]*-[:digit:]*-[:digit:]*' # Date regex
+
+# title_name : appears on the html file name and header of selected plots, change as required
+title_name <- str_remove(flnm, str_c("_", date_regex)) 
