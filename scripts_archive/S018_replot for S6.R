2f9aff73ac67e24f261164e256fa2153be404fca
ppreshant <ppresh.ant@gmail.com>
2023-11-03 05:21:27 -0500

    plotted replicates for GFP/OD ; work on fold change now

--- scripts_archive/S018_replot for S6.R
+++ scripts_archive/S018_replot for S6.R
@@ -1,6 +1,11 @@
 # S018_replot for S6.R
 
-# run_single_plate.R on 'S018_pPK6-7 selected 14_18-7-19' with sample_name = 2; till line 55
+# Read in data ----
+processed.data <- # reading in only d2 ui and i data
+  str_c('plate reader data/processed/', 'S018_pPK6-7 selected 14_18-7-19', '-processed.csv') %>% 
+  read_csv()
+# ran run_single_plate.R to get this data with user_inputs : sheet_name <- 2 to read the first 2 sheets of d2
+
 
 # sample name translation
 translate_samples <- c('pPK7_32' = 'M1',
@@ -14,7 +19,43 @@ translate_samples <- c('pPK7_32' = 'M1',
                        'MG1655' = 'E.coli'
                        )
 
+# Processing ----
 
-# subset data
+# subset data : WT, controls + top 5 mutants
 sel.data <- filter(processed.data, str_detect(Samples, 'RV01 $|rG|MG|7_21|7_6|7_28|7_32|7_17')) %>% 
-  mutate(across(Samples, ~ str_replace_all(.x, translate_samples)))
+  mutate(across(Samples, ~ str_replace_all(.x, translate_samples))) %>% # Change to more readable sample names
+  
+  mutate(sample_category = if_else(str_detect(Samples, '^M'), 'Mutants', 'Controls')) # make sample categories for facetting
+  
+
+# find baselines for plotting
+WT_baseline <- filter(sel.data, str_detect(Samples,'WT'), str_detect(Inducer,'0')) %>% # find the baseline : uninduced WT
+  pull(`GFP/OD_mean`) %>% unique  
+
+rGFP_baseline <- filter(sel.data, str_detect(Samples,'Int')) %>% # find the base value of negative control : Int(-) 
+  pull(`GFP/OD_mean`) %>% mean # take mean of induced and uninduced samples (have a slight difference only)
+
+
+# fold changes ----
+
+# plot (copy from S018 branch?)
+
+ggplot(sel.data, aes(x = Samples, y = `GFP/OD`, shape = as_factor(Inducer))) + 
+  geom_point() + 
+  scale_shape_manual(name = 'C12-AHL', values = c(1, 19), labels = c('0', '1 uM')) + # set shape 1 = inducer 0 and 19 = inducer 1 uM ;
+  
+  facet_grid(cols = vars(sample_category), scales = 'free_x') +
+  
+  ylab('GFP/OD (a.u.)') + # add y axis label 
+  
+  
+  geom_hline(yintercept = WT_baseline, linetype = 2, alpha = .5) +  # show baseline of WT negative
+  geom_hline(yintercept = rGFP_baseline, linetype = 2, alpha = .5) # show baseline of negative samples
+  # ylim(0, 2500)
+
+
+
+# top5_formatted <- {format_classic(plt_top5) + 
+
+
+ggsave('plots and data/S018_top5.pdf', top5_formatted, width = 4, height = 3)
